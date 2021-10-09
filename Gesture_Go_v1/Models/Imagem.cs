10efb15a83b8f887b7838599f4976953821e0d7b
Gui8v <gui8v.rocha8@hotmail.com>
2021-10-08 21:58:53 -0300

    Models Imagens e Sessao

---
+++ Gesture_Go_v1/Models/Imagem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gesture_Go_v1.Models
+{
+    public class Imagem
+    {
+        [Key]
+        public int img_id { get; set; }
+
+        [Required]
+        public int img_local { get; set; }
+
+        public enum tipo
+        {
+            [Display(Name = "Figura Humana")]
+            Humano = 1, 
+            Animais = 2, 
+            Estruturas = 3
+        }
+
+        [Required]
+        public tipo img_tipo { get; set; }
+
+    }
+}
---
+++ Gesture_Go_v1/Models/VMSessao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gesture_Go_v1.Models
+{
+    public class VMSessao
+    {
+        [Required]
+        public DateTime ses_timer { get; set; }
+        [Required]
+        public int ses_qtdImages { get; set; }
+        [Required]
+        public DateTime ses_Totaltimer { get; set; }
+        [Required]
+        public Imagem[] ses_images { get; set; }
+        public virtual Imagem Imagem { get; set; }
+
+
+    }
+}
