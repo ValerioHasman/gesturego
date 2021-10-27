cfd49de50d55eaa73656cf6a4d8cbe94ea665082
Gui8v <gui8v.rocha8@hotmail.com>
2021-10-27 15:15:43 -0300

    Posts

---
+++ Gesture_Go_v1/Controllers/PostsController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Gesture_Go_v1.Models;
+
+namespace Gesture_Go_v1.Controllers
+{
+    public class PostsController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Usuario);
+            return View(posts.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Posts posts = db.Posts.Find(id);
+            if (posts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posts);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.UsuarioId = new SelectList(db.Usuario, "Id", "Nome");
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Pos_id,UsuarioId,Pos_Titulo,ImagemId,Pos_imgUpload,Pos_Status")] Posts posts)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(posts);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UsuarioId = new SelectList(db.Usuario, "Id", "Nome", posts.UsuarioId);
+            return View(posts);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Posts posts = db.Posts.Find(id);
+            if (posts == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UsuarioId = new SelectList(db.Usuario, "Id", "Nome", posts.UsuarioId);
+            return View(posts);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Pos_id,UsuarioId,Pos_Titulo,ImagemId,Pos_imgUpload,Pos_Status")] Posts posts)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(posts).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UsuarioId = new SelectList(db.Usuario, "Id", "Nome", posts.UsuarioId);
+            return View(posts);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Posts posts = db.Posts.Find(id);
+            if (posts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(posts);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Posts posts = db.Posts.Find(id);
+            db.Posts.Remove(posts);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
