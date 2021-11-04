7b55c19f716785971fddee1853edda3fdcfdef31
Valério <valeriopro22.@gmail.com>
2021-11-04 10:44:42 -0300

    Preparação para recuperação

--- Gesture_Go_v1/Models/VMUsuario.cs
+++ Gesture_Go_v1/Models/VMUsuario.cs
@@ -6,43 +6,72 @@ using System.Web;
 
 namespace Gesture_Go_v1.Models
 {
-    
-        public class Cadastro
-        {
 
-            [Required]
-            [MaxLength(200)]
-            [MinLength(3)]
-            public string Nome { get; set; }
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-            [Required]
-            [DataType(DataType.Password)]
-            [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,10})", ErrorMessage = "Senha de 6 a 10 caracteres. Obrigatório Número, letra maiúscula e minúscula")]
-            public string Senha { get; set; }
-            [Required]
-            [DataType(DataType.Password)]
-            [Compare("Senha")]
-            [Display(Name = "Confirma Senha")]
-            public string ConfirmaSenha { get; set; }
+    public class Cadastro
+    {
 
-            public string ImgPerfil { get; set; }
+        [Required]
+        [MaxLength(200)]
+        [MinLength(3)]
+        public string Nome { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,10})", ErrorMessage = "Senha de 6 a 10 caracteres. Obrigatório Número, letra maiúscula e minúscula")]
+        public string Senha { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Senha")]
+        [Display(Name = "Confirma Senha")]
+        public string ConfirmaSenha { get; set; }
 
-            public int PerfilId { get; set; }
+        public string ImgPerfil { get; set; }
 
-            public virtual Perfil Perfil { get; set; }
+        public int PerfilId { get; set; }
 
-        }
+        public virtual Perfil Perfil { get; set; }
 
-        public class Acesso
-        {
-            [Required]
-            [EmailAddress]
-            public string Email { get; set; }
-            [DataType(DataType.Password)]
-            [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,12})", ErrorMessage = "A senha deve conter aos menos uma letra maiúscula, minúscula e um número. Deve ser no mínimo 6 caracteres")]
-            public string Senha { get; set; }
-        }
-    
+    }
+
+    public class Acesso
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,12})", ErrorMessage = "A senha deve conter aos menos uma letra maiúscula, minúscula e um número. Deve ser no mínimo 6 caracteres")]
+        public string Senha { get; set; }
+    }
+    public class Mensagem
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Assunto { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Mensagem")]
+        public string CorpoMsg { get; set; }
+    }
+    public class EsqueceuSenha
+    {
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+    }
+    public class RedefinirSenha
+    {
+        public string Email { get; set; }
+        public string Hash { get; set; }
+        [DataType(DataType.Password)]
+        [RegularExpression("((?=.*\\d)(?=.*[a-z])(?=.*[AZ]).{6,12})", ErrorMessage = "A senha deve conter aos menos uma letra maiúscula, minúscula e um número.Deve ser no mínimo 6 caracteres")]
+        public string Senha { get; set; }
+        [DataType(DataType.Password)]
+        [Compare("Senha")]
+        [Display(Name = "Confirma Senha")]
+        public string ConfirmaSenha { get; set; }
+    }
+
 }
